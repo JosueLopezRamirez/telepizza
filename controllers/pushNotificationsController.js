1b64505bd6f23976241b8e478292cba0e3e40a49
Noel Miranda <55200516+NoelMiranda1@users.noreply.github.com>
2022-10-17 20:09:55 -0600

    Revert "add service notification"
    This reverts commit e7d3732cea7f45e693234bc597c40eb8e21cfa3e.

--- controllers/pushNotificationsController.js
+++ controllers/pushNotificationsController.js
@@ -4,41 +4,5 @@ const https = require("https");
 console.log("LO QUE FALTA");
 
 module.exports = {
-  sendNotification(token, data) {
-    const notification = JSON.stringify({
-      to: token,
-      data: {
-        title: data.title,
-        body: data.body,
-        id_notification: data.id_notification,
-        priority: "high",
-        ttl: "4500s"
-      }
-    });
-
-    const options = {
-      hostname: "fcm.googlapis.com",
-      path: "/fcm/send",
-      method: "POST",
-      port: 443,
-      headers: {
-        "Content-Type": "application/json",
-        Authorization:
-          "key=AAAAVtzCK28:APA91bGlHK4xgu0eD3bAmpgx2qxLHGyJKynqCyH3JHl-VmuK1D7bjzNZWkU702Mrtz4UbUbEpYgQvqUiwnJcXx695ia5IvY2a_ZD9S1sk1UfHE008uEr7DObu-kZtqpT23TTP1gp_Yw5"
-      }
-    };
-
-    const req = https.request(options, (res) => {
-      console.log("STATUS CODE FIREBASE: ", res.statusCode);
-      res.on("data", (d) => {
-        process.stdout.write(d);
-      });
-    });
-
-    req.on("error", (error) => {
-      console.log("ERROR DE FIREBASE NOTIFICATION:", erro);
-    });
-    req.write(notification);
-    req.end();
-  }
+  //   sendNotification
 };
